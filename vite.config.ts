0edd7dc5be47a4c50220a0d6b4ced3de5c8345c5
Sketchyy <sketchyy@ya.ru>
2025-05-01 20:59:30 +0200

    Update eslint, fix tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), , tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
