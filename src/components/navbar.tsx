98d6f5891b801f25f52d934a6d0c344b9883c2fb
Sketchyy <sketchyy@ya.ru>
2025-05-03 20:52:16 +0200

    Use tailwind/shadcn: Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,54 +2,44 @@ import { NavLink } from "react-router-dom";
 import NavbarUser from "@/components/auth/navbar-user.tsx";
 import { ThemeSwitcher } from "@/components/theme-switcher.tsx";
 import { FatbookLogo } from "@/components/ui/fatbook-logo.tsx";
+import { cn } from "@/lib/utils";
 
-function Navbar() {
+export function Navbar() {
   const getNavLinkClass = ({ isActive }) =>
-    "navbar-item" + (isActive ? " is-active" : "");
+    cn("hover:bg-zinc-800 px-5 py-4 text-white", {
+      "bg-zinc-800": isActive,
+    });
 
   return (
     <nav
-      className="navbar is-dark hidden sm:flex"
+      className="hidden h-14 items-center bg-zinc-700 px-4 text-white sm:flex"
       role="navigation"
       aria-label="main navigation"
     >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          <FatbookLogo />
-          <h1 className="is-size-4 has-text-weight-bold ml-2">Fatbook</h1>
-        </a>
+      <a className="flex items-center gap-3 px-5 py-4" href="/">
+        <FatbookLogo />
+        <h1 className="ml-2 text-2xl font-bold text-white">Fatbook</h1>
+      </a>
 
-        <p className="navbar-item is-hidden-desktop ml-auto">
-          <ThemeSwitcher />
-        </p>
+      <div className="flex grow items-center">
+        <NavLink to="eatings" className={getNavLinkClass}>
+          Eatings
+        </NavLink>
+        <NavLink to="dishes" className={getNavLinkClass}>
+          Dishes
+        </NavLink>
+        <NavLink to="insights" className={getNavLinkClass}>
+          Insights
+        </NavLink>
+        <NavLink to="account" className={getNavLinkClass}>
+          Account
+        </NavLink>
       </div>
-      <div id="navbarMenu" className={"navbar-menu"}>
-        <div className="navbar-start">
-          <NavLink to="eatings" className={getNavLinkClass}>
-            Eatings
-          </NavLink>
-          <NavLink to="dishes" className={getNavLinkClass}>
-            Dishes
-          </NavLink>
-          <NavLink to="insights" className={getNavLinkClass}>
-            Insights
-          </NavLink>
-          <NavLink to="account" className={getNavLinkClass}>
-            Account
-          </NavLink>
-        </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item is-hidden-touch">
-            <ThemeSwitcher />
-          </div>
-          <div className="navbar-item">
-            <NavbarUser />
-          </div>
-        </div>
+      <div className="flex items-center gap-5">
+        <ThemeSwitcher />
+        <NavbarUser />
       </div>
     </nav>
   );
 }
-
-export default Navbar;
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "@/components/navbar.tsx";
+import { Navbar } from "@/components/navbar.tsx";
 import { useIsFetching } from "@tanstack/react-query";
 import { clsx } from "clsx";
 import { MobileBottomNav } from "@/components/mobile-bottom-nav.tsx";
