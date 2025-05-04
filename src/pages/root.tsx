2e236600dcc969dc6ea025cd6d3777b0492b6581
Sketchyy <sketchyy@ya.ru>
2025-05-04 17:08:22 +0200

    tailwind: Remove unused libs

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/navbar.tsx";
 import { useIsFetching } from "@tanstack/react-query";
-import { clsx } from "clsx";
 import { MobileBottomNav } from "@/components/mobile-bottom-nav.tsx";
+import { cn } from "@/lib/utils.ts";
 
 export function Root() {
   const fetchingCount = useIsFetching({ queryKey: ["dishes"] });
@@ -12,7 +12,7 @@ export function Root() {
     <>
       <Navbar />
       <div
-        className={clsx("mb-18 pb-4 sm:mb-0 sm:py-4", {
+        className={cn("mb-18 pb-4 sm:mb-0 sm:py-4", {
           loading: isLoading,
         })}
       >
