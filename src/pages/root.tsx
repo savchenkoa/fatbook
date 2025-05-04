f137ae33d3c52f218aab8091845a86d91cef3397
Sketchyy <sketchyy@ya.ru>
2025-05-04 22:31:39 +0200

    dependencies: fix mobile toaster

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -4,10 +4,8 @@ import { useIsFetching } from "@tanstack/react-query";
 import { MobileBottomNav } from "@/components/mobile-bottom-nav.tsx";
 import { cn } from "@/lib/utils.ts";
 import { Toaster } from "@/components/ui/sonner.tsx";
-import { useIsTouchDevice } from "@/hooks/use-is-touch-device.ts";
 
 export function Root() {
-  const isTouchDevice = useIsTouchDevice();
   const fetchingCount = useIsFetching({ queryKey: ["dishes"] });
   const isLoading = fetchingCount > 0;
 
@@ -25,7 +23,7 @@ export function Root() {
       <Toaster
         closeButton
         richColors
-        position={isTouchDevice ? "top-center" : "top-right"}
+        position="top-right"
         toastOptions={{
           style: {
             padding: "1.5rem",
