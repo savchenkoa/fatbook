945c2aa99250d310e3d49db3cb7068ed569c8a8d
Sketchyy <sketchyy@ya.ru>
2025-05-11 12:08:57 +0200

    Add buttons to adjust portion size in the selector

--- src/features/dish-portions-form/components/portion-size-selector.tsx
+++ src/features/dish-portions-form/components/portion-size-selector.tsx
@@ -10,7 +10,7 @@ import {
 import { Button } from "@/components/ui/button.tsx";
 import { DishPortion } from "@/types/dish-portion.ts";
 import { Input } from "@/components/ui/input.tsx";
-import { LucidePlus, LucideTrash } from "lucide-react";
+import { LucideMinus, LucidePlus, LucideTrash } from "lucide-react";
 import { Label } from "@/components/ui/label.tsx";
 import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
 import { DishTitle } from "@/components/ui/dish-title.tsx";
@@ -78,45 +78,57 @@ export function PortionSizeSelector({
             <DrawerTitle className="mb-4">
               <div className="relative w-full text-center text-2xl">
                 <DishTitle dish={dishPortion.dish} className="justify-center" />
-                {isEditing && (
-                  <Button
-                    onClick={handleDeleteClick}
-                    variant="ghost"
-                    size="icon"
-                    className="absolute top-1/2 right-0 translate-y-[-50%] text-red-500 hover:bg-red-100 hover:text-red-500"
-                  >
-                    <LucideTrash />
-                  </Button>
-                )}
               </div>
             </DrawerTitle>
-            <DrawerDescription>
-              <Label htmlFor="portion-size-input">Portion (g.)</Label>
-              <Input
-                autoFocus
-                type="number"
-                id="portion-size-input"
-                placeholder="gramm"
-                className="mt-2 px-6 py-10 text-center text-4xl sm:py-6 sm:text-2xl"
-                value={portionSize === undefined ? "" : portionSize}
-                onChange={handleInputChange}
-                onFocus={(e) => e.target.select()}
-                onKeyDown={handleKeyDown}
-              />
-              {/*<Slider*/}
-              {/*  defaultValue={[portionSize ?? 100]}*/}
-              {/*  onValueChange={(value) => setPortionSize(value[0])}*/}
-              {/*  min={0}*/}
-              {/*  max={300}*/}
-              {/*  step={1}*/}
-              {/*  className="mt-10 mb-5"*/}
-              {/*/>*/}
+            <DrawerDescription className="flex items-end justify-between gap-4">
+              <Button
+                variant="outline"
+                size="icon"
+                className="size-[50px]"
+                onClick={() => setPortionSize((v) => (v ? v - 1 : -1))}
+              >
+                <LucideMinus />
+              </Button>
+              <div className="flex-1">
+                <Label htmlFor="portion-size-input">Portion (g.)</Label>
+                <Input
+                  autoFocus
+                  type="number"
+                  id="portion-size-input"
+                  placeholder="gramm"
+                  className="mt-2 px-6 py-10 text-center text-4xl sm:py-6 sm:text-2xl"
+                  value={portionSize === undefined ? "" : portionSize}
+                  onChange={handleInputChange}
+                  onFocus={(e) => e.target.select()}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="size-[50px]"
+                onClick={() => setPortionSize((v) => (v ? v + 1 : 1))}
+              >
+                <LucidePlus />
+              </Button>
             </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
-            <Button onClick={handleSubmitClick} className="py-6">
-              {!isEditing && <LucidePlus />} {isEditing ? "Save" : "Add"}
-            </Button>
+            <div className="mt-4 flex items-center gap-4">
+              {isEditing && (
+                <Button
+                  onClick={handleDeleteClick}
+                  variant="outline"
+                  className="flex-1 border-red-500 text-red-500 hover:bg-red-100 hover:text-red-500"
+                >
+                  <LucideTrash className="mr-2" /> Delete
+                </Button>
+              )}
+              <Button onClick={handleSubmitClick} className="flex-1">
+                {!isEditing && <LucidePlus className="mr-2" />}{" "}
+                {isEditing ? "Save" : "Add"}
+              </Button>
+            </div>
             <DrawerClose asChild>
               <Button variant="ghost">Cancel</Button>
             </DrawerClose>
