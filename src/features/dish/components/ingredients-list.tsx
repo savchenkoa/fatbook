d6297d7f910e441a4c0cb1e65bd30e2dcdea799b
Sketchyy <sketchyy@ya.ru>
2025-07-20 18:55:45 +0200

    Fix flaky test + improve slow internet UX

--- src/features/dish/components/ingredients-list.tsx
+++ src/features/dish/components/ingredients-list.tsx
@@ -3,6 +3,7 @@ import { Dish } from "@/types/dish.ts";
 import { DishPortion } from "@/types/dish-portion.ts";
 import { useIngredientMutations } from "../hooks/use-ingredients-mutations.ts";
 import { Box } from "@/components/ui/box.tsx";
+import { useEffect } from "react";
 
 type Props = {
   dish: Dish;
@@ -10,9 +11,18 @@ type Props = {
 };
 
 export function IngredientsList({ dish, isDishShared }: Props) {
-  const { updateIngredient, removeIngredient, selectedPortions } =
-    useIngredientMutations(dish, dish.ingredients);
+  const {
+    updateIngredient,
+    removeIngredient,
+    selectedPortions,
+    setSelectedPortions,
+  } = useIngredientMutations(dish, dish.ingredients);
   const ingredients = selectedPortions.map((p) => ({ ...p, selected: false }));
+
+  useEffect(() => {
+    // Force sync with BE data, helps to avoid stale optimistic data with slow internet
+    setSelectedPortions(dish.ingredients);
+  }, [dish, setSelectedPortions]);
 
   const handleUpgradeIngredient = async (ingredient: DishPortion) => {
     updateIngredient.mutate(ingredient);
